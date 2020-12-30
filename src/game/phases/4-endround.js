3ea3f00afa86a84a58c1a8a56b4a0e9ac3b597bb
Robert Preus-MacLaren <rob@neontribe.co.uk>
2020-12-30 22:06:36 +0000

    double score

--- src/game/phases/4-endround.js
+++ src/game/phases/4-endround.js
@@ -1,3 +1,4 @@
+import omit from 'lodash.omit';
 import { addCards } from "../cards";
 import { ssm } from "../ssm";
 
@@ -10,9 +11,20 @@ export const endround = {
   onEnd: (G, ctx) => {
     const score = {};
     const lastScores = G.scores[G.scores.length - 1] || {};
-    Object.entries(G.secret.hands).forEach(([playerID, cards]) => {
-      score[playerID] = (lastScores[playerID] || 0) + addCards(cards);
+    const firstOutScore = addCards(G.secret.hands[G.playerFirstOut]);
+    let hasLowest = true;
+
+    Object.entries(omit(G.secret.hands, G.playerFirstOut)).forEach(([playerID, cards]) => {
+      const s = addCards(cards);
+
+      if (s <= firstOutScore && hasLowest) {
+        hasLowest = false;
+      }
+
+      score[playerID] = lastScores[playerID] + s;
     });
+
+    score[G.playerFirstOut] = hasLowest ? firstOutScore : firstOutScore * 2;
 
     G.scores.push(score);
   },
